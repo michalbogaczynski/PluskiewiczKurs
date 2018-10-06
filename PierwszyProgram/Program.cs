fb6a942904a2fc4a9404e31a201b4cc3882701ac
michalbogaczynski <bogaczynski.m@gmail.com>
2018-10-06 21:18:43 +0200

    Podstawowa składnia (zmienne, metodey, klasa, instancja klasy)

--- PierwszyProgram/Program.cs
+++ PierwszyProgram/Program.cs
@@ -10,10 +10,37 @@ namespace PierwszyProgram
     {
         static void Main(string[] args)
         {
-            /* Pierwszy program */                              
-
-            Console.WriteLine("Witaj świecie");
+            Rectangle r = new Rectangle();
+            r.AkceptujDetale();
+            r.WyswietlPodsumowanie();
             Console.ReadKey();
+
         }
     }
+
+    class Rectangle
+    {
+        double dlugosc;
+        double szerokosc;
+
+        public void AkceptujDetale()
+        {
+            dlugosc = 10;
+            szerokosc = 20;
+        }
+
+        public double ObliczPowierzchnie()
+        {
+            return dlugosc * szerokosc;
+        }
+
+        public void WyswietlPodsumowanie()
+        {
+            Console.WriteLine("Długość: {0}", dlugosc);
+            Console.WriteLine("Szerokość: {0}", szerokosc);
+            Console.WriteLine("Powierzchnia: {0}", ObliczPowierzchnie());
+        }
+
+    }
+
 }
