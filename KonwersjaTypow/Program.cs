6f6b2fc2ba86a4b78b1299becc41fda5e42589d7
michalbogaczynski <bogaczynski.m@gmail.com>
2018-10-07 13:25:27 +0200

    Konwersja typów jawna/niejawna

---
+++ KonwersjaTypow/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonwersjaTypow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //niejawna konwersja danych (bezpieczna)
+            int wartosc1 = 0;
+            byte wartosc2 = 15;
+
+            wartosc1 = wartosc2; //wartosc2 przypisana jest do wartosci1 czyli wartosc1=15
+            Console.WriteLine(wartosc2);
+            Console.WriteLine(34 + 34.5); //z automatu wartość bedzie zmiennoprzecinkowa
+            
+
+            //konwersja jawna
+
+            double liczba = 3765.47;
+            int i;
+            i = (int)liczba;
+            Console.WriteLine(i);
+
+
+            int j = 32;
+            float f = 32.005f;
+            double d = 1234 / 5678;
+            bool b = false;
+            Console.WriteLine(j.ToString());
+            Console.WriteLine(f.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TypyDanych/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypyDanych
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rozmiar zmiennej typu int: {0}", sizeof(bool));
+            Console.ReadKey();
+        }
+    }
+}
